3cbc6cd94429e5a13720f4a28931f03cd3085cfa
JustDMare <just@dmare.me>
2023-01-07 11:28:23 +0100

    Compiled

--- lib/blocks/heading-big.d.ts
+++ lib/blocks/heading-big.d.ts
@@ -1,4 +1,4 @@
-import { type Block } from "./block";
+import type { Block } from "./block";
 export interface HeadingBigBlock extends Block {
     type: "heading_big";
 }
--- lib/blocks/heading-medium.d.ts
+++ lib/blocks/heading-medium.d.ts
@@ -1,4 +1,4 @@
-import { type Block } from "./block";
+import type { Block } from "./block";
 export interface HeadingMediumBlock extends Block {
     type: "heading_medium";
 }
