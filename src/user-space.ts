ba587fcfbe14a800644002997b41550625ff2c98
JustDMare <just@dmare.me>
2023-01-07 13:33:26 +0100

    Small fix: UserSpaceSchema _id -> string

--- src/user-space.ts
+++ src/user-space.ts
@@ -2,13 +2,13 @@ import type { Folder } from "./folder";
 import type { NavigationNoteReference } from "./note-reference";
 
 export interface UserSpaceSchema {
-	_id: Object;
-	userToken: string;
+  _id: string;
+  userToken: string;
 }
 
 export interface UserSpace extends UserSpaceSchema {
-	content: {
-		folders: Folder[];
-		notes: NavigationNoteReference[];
-	};
+  content: {
+    folders: Folder[];
+    notes: NavigationNoteReference[];
+  };
 }
